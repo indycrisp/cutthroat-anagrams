318272cdaf03e44482c47ecb1d7262466fa60951
Greg Leinwand <gregleinwand@gregs-mbp.lan>
2018-01-02 20:30:48 -0500

    production configs

--- config/connections.js
+++ config/connections.js
@@ -21,32 +21,33 @@
 
 module.exports.connections = {
 
-  /***************************************************************************
-  *                                                                          *
-  * Local disk storage for DEVELOPMENT ONLY                                  *
-  *                                                                          *
-  * Installed by default.                                                    *
-  *                                                                          *
-  ***************************************************************************/
-  localDiskDb: {
-    adapter: 'sails-disk'
-  },
+	/***************************************************************************
+	*                                                                          *
+	* Local disk storage for DEVELOPMENT ONLY                                  *
+	*                                                                          *
+	* Installed by default.                                                    *
+	*                                                                          *
+	***************************************************************************/
+  	localDiskDb: {
+		adapter: 'sails-disk'
+	},
 
-  /***************************************************************************
-  *                                                                          *
-  * MySQL is the world's most popular relational database.                   *
-  * http://en.wikipedia.org/wiki/MySQL                                       *
-  *                                                                          *
-  * Run: npm install sails-mysql                                             *
-  *                                                                          *
-  ***************************************************************************/
-  // someMysqlServer: {
-  //   adapter: 'sails-mysql',
-  //   host: 'YOUR_MYSQL_SERVER_HOSTNAME_OR_IP_ADDRESS',
-  //   user: 'YOUR_MYSQL_USER', //optional
-  //   password: 'YOUR_MYSQL_PASSWORD', //optional
-  //   database: 'YOUR_MYSQL_DB' //optional
-  // },
+	/***************************************************************************
+	*                                                                          *
+	* MySQL is the world's most popular relational database.                   *
+	* http://en.wikipedia.org/wiki/MySQL                                       *
+	*                                                                          *
+	* Run: npm install sails-mysql                                             *
+	*                                                                          *
+	***************************************************************************/
+	mysql: {
+		adapter: 'sails-mysql',
+		host: 'localhost',
+		port: 3306,
+		//user: 'gleinwand', //optional
+		//password: 'catbutt1', //optional
+		//database: 'YOUR_MYSQL_DB' //optional
+	},
 
   /***************************************************************************
   *                                                                          *
--- config/env/development.js
+++ config/env/development.js
@@ -12,13 +12,12 @@
 
 module.exports = {
 
-  /***************************************************************************
-   * Set the default database connection for models in the development       *
-   * environment (see config/connections.js and config/models.js )           *
-   ***************************************************************************/
-
-  // models: {
-  //   connection: 'someMongodbServer'
-  // }
-
+	/***************************************************************************
+	* Set the default database connection for models in the development       *
+	* environment (see config/connections.js and config/models.js )           *
+	***************************************************************************/
+	models: {
+		connection: 'localDiskDb',
+		migrate: 'alter'
+	}
 };
