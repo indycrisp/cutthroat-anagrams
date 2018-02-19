285c1f4b35678a9a51256797683b034675df21be
Greg Leinwand <gleinwand@bitbucket.org>
2018-02-19 17:35:58 -0500

    www?

--- www/templates/templates.js
+++
@@ -1,6 +0,0 @@
-(function() {
-  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
-templates['test'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "{\"version\":3,\"sources\":[\"templates/templates.js\"],\"names\":[\"compiler\",\"main\",\"useData\"],\"mappings\":\"2GAAAA,UAAA,EAAA,YAAAC,mCAAAC,SAAA\",\"file\":\"templates/test.hbs\"}";
-},"useData":true});
-})();
