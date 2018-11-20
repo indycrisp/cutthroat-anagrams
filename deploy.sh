160d8f7fd936aee5ee12b6195da0e312306ea41e
Greg Leinwand <gleinwand@bitbucket.org>
2018-11-20 15:40:34 -0500

    deploy changes

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,9 @@
 
 stopall="forever stopall"
 grunt="./node_modules/grunt/bin/grunt prod"
-jst="./node_modules/grunt/.bin/grunt jst"
-hashres="./node_modules/grunt/.bin/grunt hashres"
-webpack="./node_modules/webpack/.bin/webpack"
+jst="./node_modules/.bin/grunt jst"
+hashres="./node_modules/.bin/grunt hashres"
+webpack="./node_modules/.bin/webpack"
 run="forever start app.js --prod"
 $stopall
 $grunt
