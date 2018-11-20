34b86b7c04951820f6080443f14666682378e31d
Greg Leinwand <gleinwand@bitbucket.org>
2018-11-20 12:26:20 -0500

    cache busting

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 var path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	entry: path.resolve(__dirname, 'assets/js/app.js'),
 	output: {
 		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'assets/js/dist')
+		path: path.resolve(__dirname, 'assets/js/dist'),
+		publicPath: '/js/dist/'
 	},
 	module: {
 		rules: [
@@ -16,5 +18,12 @@ module.exports = {
 	},
 	node: {
 		fs: "empty"
-	}
+	},
+	plugins: [
+		new HTMLWebpackPlugin({
+			template: '!!raw-loader!./views/layout_base.ejs',
+			filename: '../../../views/layout.ejs',
+			hash: true
+		})
+	]
 };
