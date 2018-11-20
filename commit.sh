c9a760feadaaf108ca7a341ec95d4057a2401842
Greg Leinwand <gleinwand@bitbucket.org>
2018-11-20 18:50:13 -0500

    default

--- commit.sh
+++ commit.sh
@@ -1,6 +1,7 @@
-./node_modules/grunt/.bin/grunt jst
-./node_modules/grunt/.bin/grunt hashres
-./node_modules/grunt/.bin/webpack
+cd ~/cutthroat-anagrams
+node_modules/grunt/.bin/grunt jst
+node_modules/grunt/.bin/grunt hashres
+node_modules/grunt/.bin/webpack
 git add .
 git commit -m "default"
 git push origin master &
