61a18c447ced6edc1f7a6a3fb89043b9328ee478
Greg Leinwand <gleinwand@bitbucket.org>
2018-11-20 15:53:29 -0500

    cache test again

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,7 @@
 
 stopall="forever stopall"
 grunt="./node_modules/grunt/bin/grunt prod"
-#jst="./node_modules/.bin/grunt jst"
-#hashres="./node_modules/.bin/grunt hashres"
-#webpack="./node_modules/.bin/webpack"
 run="forever start app.js --prod"
 $stopall
 $grunt
-#$webpack
-#$jst
-#$hashres
 $run &
