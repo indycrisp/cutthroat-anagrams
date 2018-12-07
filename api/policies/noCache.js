7741bbd6e60fbc0cc34909671f9c07931fa9c7f2
Greg Leinwand <gleinwand@bitbucket.org>
2018-12-06 21:33:05 -0500

    undo first cache disabling try

--- api/policies/noCache.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(req, res, next) {
-	sails.log.info("Applying cache disabling");
-	res.setHeader('Cache-Control', 'no-cache');
-	res.setHeader('Expires', '-1');
-	res.setHeader('Pragma', 'no-cache');
-	next();
-};
